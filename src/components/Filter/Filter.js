f06fc1c63689c7fb8bbcf225e46d2a6c7b04f107
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-10-10 02:18:52 +0300

    выполняет задание2

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Filter.module.css";
+import PropTypes from "prop-types";
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <label className={styles.label}>
+      Find contacts by name
+      <input
+        className={styles.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+Filter.defaultProps = {
+  value: "",
+};
+
+Filter.propTypes = {
+  value: PropTypes.string,
+};
+
+export default Filter;
